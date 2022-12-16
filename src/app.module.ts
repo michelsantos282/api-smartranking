4dbad023cb8cecbcf831ee7d67a1dfe938a29e39
Michel Santos <michel.santos@webjump.com.br>
2022-12-16 10:46:20 -0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { CategoriasModule } from './categorias/categorias.module';
 import { DesafiosModule } from './desafios/desafios.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://mdsantos:michel1210@cluster0.u3y3bak.mongodb.net/smartranking?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb+srv://${configService.get<string>(
+          'MONGO_DB_USER',
+        )}:${configService.get<string>(
+          'MONGO_DB_PASS',
+        )}@cluster0.u3y3bak.mongodb.net/smartranking?retryWrites=true&w=majority`,
+      }),
+      inject: [ConfigService],
+    }),
     JogadoresModule,
     CategoriasModule,
     DesafiosModule,
